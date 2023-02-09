24bdc58e4478284e10c751b86145264a7c3add1b
wjdals95 <wjdals992@naver.com>
2023-02-09 17:35:50 +0900

    세부사항페이지에서 데이터 출력하기 & NotFound 페이지 추가

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
 import Layout from "./components/layout/Layout";
-
+import NotFound from "./pages/NotFound";
 function App() {
   return (
     <Layout>
@@ -20,6 +20,11 @@ function App() {
         </Route>
         <Route path="/new-quotes">
           <NewQuote />
+        </Route>
+        {/* 위의 경로와 일치하지 않는 모든 URL과 아래의 라우트가 일치되며
+         Not Found페이지를 렌더링 하게 된다. */}
+        <Route path='*'>
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -2,13 +2,29 @@ import React, { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
-
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "JM",
+    text: "Learning React is fun!",
+  },
+  {
+    id: "q2",
+    author: "CJM",
+    text: "Learning React is great!",
+  },
+];
 const QuoteDetail = () => {
   const params = useParams();
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if(!quote) {
+    return <p>No quote found!</p>
+  }
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+     <HighlightedQuote text={quote.text} author={quote.author} />
       <Route to={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
